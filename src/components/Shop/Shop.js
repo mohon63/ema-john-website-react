c7eb466b6e75f0a3814ceef0fdc81ed1c6c0c8a3
mohon63 <mohonhossen63@gmail.com>
2021-09-24 01:00:28 +0600

    header menu and gird

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Shop.css'
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('./products.JSON')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
     return (
-        <div>
-            <h2>This is Shop</h2>
+        <div className="shop-container">
+            <div className="product-container">
+                {
+                    products.map(product => console.log(product))
+                }
+                <h3>Product: {products.length}</h3>
+            </div>
+            <div className="cart-container">
+                <h3>Order Summary</h3>
+                <h5>Items Ordered: </h5>
+            </div>
+
         </div>
     );
 };
